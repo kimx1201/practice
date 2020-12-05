a3319362d87e4171fbea26271ad7474a1eebd321
kimx1201 <iloveyoumoretoday@gmail.com>
2020-12-05 14:30:21 +0900

    python practice

--- 201205.py
+++ 201205.py
@@ -27,3 +27,22 @@ elif s < .6:
 else:
 	x ="Out of Range"
 print (x)
+
+largest = None 
+smallest = None 
+while True: 
+  num = input("Enter a number: ") 
+  if num == "done" : 
+    break 
+  try: 
+    num1 = int(num) 
+  except: 
+    print("Invalid input") 
+    continue 
+  if largest is None or num1 > largest:
+    largest = num1 
+  if smallest is None or num1 < smallest:
+    smallest = num1 
+
+print("Maximum is", largest) 
+print("Minimum is", smallest)
